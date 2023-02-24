b4a997b5b3fb362cee387738b301b5cc7fa055a2
Klein254 <rayobiero62@gmail.com>
2023-02-24 10:40:28 +0300

    first

---
+++ djangoelvis/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def server(request):
+    return render(request, "server.html")
